7803391bad3a3d9d6c400d3e56e4d315dc2817a1
yunnozee <nohyunji127@gmail.com>
2025-08-20 18:41:48 +0900

    기본 이동처리 / 터치 이동 확인 필요

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,65 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private static readonly int RUN = Animator.StringToHash("RUN");
+    private PlayerInputActions playerInputActions;
+    private Animator animator;
+    private VirtualJoystick joystick; 
+    
+    [SerializeField] private float speed = 5f;
+    
+    public Vector2 MoveInput { get; private set; }
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        joystick = GetComponent<VirtualJoystick>();
+        animator = GetComponent<Animator>();
+        
+        animator.SetTrigger(RUN);
+    }
+
+    private void OnEnable()
+    {
+        playerInputActions.Enable();
+
+        playerInputActions.Player.Move.performed += OnMove;
+        playerInputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.Player.Move.performed -= OnMove;
+        playerInputActions.Player.Move.canceled -= OnMove;
+
+        playerInputActions.Disable();
+    }
+    
+    private void Update()
+    {
+        // PC/패드 입력
+        Vector2 inputFromDevice = MoveInput;
+
+        // 모바일 조이스틱 입력 (없으면 Vector2.zero)
+        if (joystick != null && joystick.Input != Vector2.zero)
+            inputFromDevice = joystick.Input;
+
+        // 이동 처리
+        Vector3 dir = new Vector3(inputFromDevice.x, 0, inputFromDevice.y);
+        transform.position += dir * (speed * Time.deltaTime);
+    }
+    
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        MoveInput = ctx.ReadValue<Vector2>();
+        Debug.Log($"MoveInput: {MoveInput}");
+    }
+}
+
+
+
---
+++ Assets/Scripts/Player/VirtualJoystick.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private RectTransform handle;
+    [SerializeField] private float handleRange = 100f;
+
+    private Vector2 inputVector;
+    private Vector2 startPosition;
+
+    public Vector2 Input => inputVector;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startPosition = eventData.position; // 터치 시작 위치 = 원점
+        handle.position = startPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 offset = eventData.position - startPosition;
+        inputVector = Vector2.ClampMagnitude(offset / handleRange, 1f);
+        handle.position = startPosition + inputVector * handleRange;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector2.zero;
+        handle.position = startPosition; // 원래 자리로 복귀
+    }
+}
